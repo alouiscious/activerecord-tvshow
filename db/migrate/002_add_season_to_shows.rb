13d6963dba47b39223118015f9d04292bc52022f
Alouiscious Media <info@reneesoul.com>
2019-10-09 01:38:55 -0700

    git even

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,7 +1,6 @@
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
-    add_column :shows do |c|
-      c.string :season
-    end
+    add_column :shows, :season, :string 
+    
   end
 end
